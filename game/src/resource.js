b3575b1d30691a5eb066d6d62612e8c2311ffafd
vitrum.zhu <vitrum.cn@gmail.com>
2014-02-16 15:08:38 +0800

    add cocos2d-html5 code

---
+++ game/src/resource.js
@@ -0,0 +1,22 @@
+var s_HelloWorld = "HelloWorld.jpg";
+var s_CloseNormal = "CloseNormal.png";
+var s_CloseSelected = "CloseSelected.png";
+var s_BackGround = "background.png";
+
+var g_resources = [
+    //image
+    {src:s_HelloWorld},
+    {src:s_CloseNormal},
+    {src:s_CloseSelected},
+    {src:s_BackGround},
+
+    //plist
+
+    //fnt
+
+    //tmx
+
+    //bgm
+
+    //effect
+];
