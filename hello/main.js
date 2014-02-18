666ce039a5f9c638726d43fb9def6c52f31b2485
vitrum.zhu <vitrum.cn@gmail.com>
2014-02-18 09:24:27 +0800

    last update

--- hello/main.js
+++ hello/main.js
@@ -43,7 +43,7 @@ var cocos2dApp = cc.Application.extend({
         // initialize director
         var director = cc.Director.getInstance();
 
-        cc.EGLView.getInstance().resizeWithBrowserSize(true);
+        cc.EGLView.getInstance().resizeWithBrowserSize(false);
         cc.EGLView.getInstance().setDesignResolutionSize(320, 480, cc.RESOLUTION_POLICY.SHOW_ALL);
 
         // turn on display FPS
