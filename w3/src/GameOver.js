a22e26d92b8be588fca4b89897b216eb726df48d
vitrum.zhu <vitrum.cn@gmail.com>
2014-03-05 10:13:34 +0800

    add update share function

--- w3/src/GameOver.js
+++ w3/src/GameOver.js
@@ -10,65 +10,65 @@ shareLayerCloseer.addEventListener("click", function (event) {
 var rankLayerList = document.getElementById('toplist');
 var textRankList = '';
 
-function generatRankList(data){
-    var htmlCode = '<table>';
-// index: 0
-// name: "axing2"
-// value01: 123456789
-    for (var i = 0; i < data.length ; i++) {
-        cc.log(data[i]);
-        htmlCode = htmlCode + '<tr><td class="number">' + (data[i].index + 1) + '</td>';
-        htmlCode = htmlCode + '<td class="name">' + data[i].name + '</td>';
-        htmlCode = htmlCode + '<td class="value">' + data[i].value01 + '</td>';
 
-    }
-    htmlCode = htmlCode + '</tr></table>'
-
-    return htmlCode;
-}
 
 function jsonpCallback(data){
     // cc.log('jsonpCallback');
     // cc.log(data);
     MW.TOP10_t = data;
-    // if(!MW.TOP10_t.game_name){
-    //     cc.log(rankLayerList);
-    //     rankLayerList.innerHTML = MW.TOP10_t ;
-    // }else{
-    //      cc.log(MW.TOP10_t);
-    // }
+    if(!MW.TOP10_t.game_name){
+        cc.log(rankLayerList);
+        rankLayerList.innerHTML = MW.TOP10_t ;
+    }else{
+         cc.log(MW.TOP10_t);
+    }
     // cc.log(MW.TOP10_t);
     // cc.log(MW.TOP10);
     // var 
-    cc.log(MW.TOP10);
-    MW.TOP10 = data;
-    // MW.TOP10 = listSortBy(MW.TOP10_t, 'value01', 'desc');
-    cc.log(MW.TOP10);
-    var newlist = generatRankList(MW.TOP10);
-    rankLayerList.innerHTML = newlist;
-    cc.log(newlist);
+    cc.log(MW.TOP10[9]);
+    MW.TOP10 = listSortBy(MW.TOP10_t, 'value01', 'desc');
+    cc.log(MW.TOP10[9]);
+    for (var i = MW.TOP10.length - 1; i >= 0; i--) {
+        
+    }
     // cc.log(a[0]);
     // var b = listSortBy(MW.GiftRecord , 'age',  'desc');
     // // cc.log(b);
 }
 function jsonpPostback(data){
     MW.TOP10_t = data;
-    // cc.log(data);
-    // cc.log(MW.TOP10_t.global_ranking);
-    // cc.log(MW.TOP10_t.img-code);
-    // MW.TOP10 = listSortBy(MW.TOP10_t.global_ranking, 'value01', 'desc');
-    // cc.log(MW.TOP10[9]);
-    MW.TOP10 = MW.TOP10_t.global_ranking;
-
-    
-    if(!MW.TOP10_t.game_name){
-    //     cc.log(rankLayerList);
-    //     rankLayerList.innerHTML = MW.TOP10_t ;
-    }else{
-    //      cc.log(MW.TOP10_t);
-        var newlist = generatRankList(MW.TOP10);
-        rankLayerList.innerHTML = newlist;
+    cc.log(data);
+    cc.log(MW.TOP10_t.global_ranking);
+    cc.log(MW.TOP10_t.img-code);
+    MW.TOP10 = listSortBy(MW.TOP10_t.global_ranking, 'value01', 'desc');
+    cc.log(MW.TOP10[9]);
+    if(!MW.gameCode){
+        MW.gameCode = MW.TOP10_t.img-code;
     }
+    updateShareLink();
+}
+
+function updateShareLink(){
+
+    cc.log(MW.TOP10[9].value01);
+
+    var shareWord = 'http://service.weibo.com/share/share.php?title=';
+    shareWord = shareWord + '%e6%88%91%e5%9c%a8%40%e5%be%b7%e5%9b%bd%e9%a9%ac%e7%89%8c%e8%bd%ae%e8%83%8e+%23%e8%b5%9b%e8%bd%a6%e6%b8%b8%e6%88%8f%23%e4%b8%ad%e9%a9%b0%e9%aa%8b%e4%ba%86';
+    shareWord = shareWord + MW.SCORE ;
+    shareWord = shareWord + '%e7%b1%b3%ef%bc%8c%e5%b9%b6%e6%88%90%e5%8a%9f%e7%95%99%e5%90%8d%e8%8b%b1%e9%9b%84%e6%a6%9c%e3%80%82%e6%83%b3%e7%9f%a5%e9%81%93%e4%bd%a0%e7%9a%84%e9%a9%be%e9%a9%b6%e9%a3%8e%e6%a0%bc%e5%90%97%ef%bc%9f%e6%83%b3%e8%b6%85%e8%bf%87%e6%88%91%ef%bc%8c%e4%b8%8e%e6%88%91%e4%b8%80%e8%be%83%e9%ab%98%e4%b8%8b%e5%90%97%ef%bc%9f%e8%af%b7%e7%82%b9%e5%87%bb%e6%b8%b8%e6%88%8f%e9%93%be%e6%8e%a5%ef%bc%8c%e6%88%96%e6%89%ab%e6%8f%8f%e4%ba%8c%e7%bb%b4%e7%a0%81%ef%bc%8c%e5%bc%80%e5%90%af%e7%b4%a7%e5%bc%a0%e3%80%81%e5%88%ba%e6%bf%80%e7%9a%84%e6%8c%91%e6%88%98%e5%90%a7%ef%bc%81';
+    shareWord = shareWord + '&source=bookmark&appkey=&ralateUid=&pic=http%3a%2f%2fwegift.reconnectplatform.com%2fracegame%2foperator%3faction%3dget_score_by_img_code%26img-code%3d';
+    shareWord = shareWord + MW.gameCode;
+    shareWord = shareWord + '&url=http%3a%2f%2fthenftgame.github.io%2frace%2fw3%2f';
+
+
+        console.log(shareWord);
+        // $('.getmore').attr('href', shareWord);
+    var shareLink = document.getElementsByClassName('js-sharelink')[0];
+    shareLink.setAttribute('href', shareWord);
+    // 我在@德国马牌轮胎 #赛车游戏#中驰骋了aaa米
+    cc.log('title:' + document.title );
+    document.title = "我在@德国马牌轮胎 #赛车游戏#中驰骋了" + MW.SCORE +"米";
+
 }
 
 var script = document.createElement('script');
@@ -129,35 +129,38 @@ var GameOver = cc.Layer.extend({
             // this.addChild(logo,10,1);
 
 
+            var gamePost = document.createElement('script');
+            gamePost.src = 'http://wegift.reconnectplatform.com/racegame/operator?action=submit_game_score&game_name=RaceGame&player=axing2&score='+ MW.SCORE +'&type=a&refresh=1&jsoncallback=jsonpPostback';
+            document.getElementsByTagName('head')[0].appendChild(gamePost);
+
+
 
             var flare = cc.Sprite.create(res.flare_jpg);
             this.addChild(flare);
             flare.setVisible(false);
 
-            cc.log(MW.TOP10[9].value01);
+            // cc.log(MW.TOP10[9].value01);
 
-            var shareWord = 'http://service.weibo.com/share/share.php?title=';
-            shareWord = shareWord + '%e6%88%91%e5%9c%a8%40%e5%be%b7%e5%9b%bd%e9%a9%ac%e7%89%8c%e8%bd%ae%e8%83%8e+%23%e8%b5%9b%e8%bd%a6%e6%b8%b8%e6%88%8f%23%e4%b8%ad%e9%a9%b0%e9%aa%8b%e4%ba%86';
-            shareWord = shareWord + MW.SCORE ;
-            shareWord = shareWord + '%e7%b1%b3%ef%bc%8c%e5%b9%b6%e6%88%90%e5%8a%9f%e7%95%99%e5%90%8d%e8%8b%b1%e9%9b%84%e6%a6%9c%e3%80%82%e6%83%b3%e7%9f%a5%e9%81%93%e4%bd%a0%e7%9a%84%e9%a9%be%e9%a9%b6%e9%a3%8e%e6%a0%bc%e5%90%97%ef%bc%9f%e6%83%b3%e8%b6%85%e8%bf%87%e6%88%91%ef%bc%8c%e4%b8%8e%e6%88%91%e4%b8%80%e8%be%83%e9%ab%98%e4%b8%8b%e5%90%97%ef%bc%9f%e8%af%b7%e7%82%b9%e5%87%bb%e6%b8%b8%e6%88%8f%e9%93%be%e6%8e%a5%ef%bc%8c%e6%88%96%e6%89%ab%e6%8f%8f%e4%ba%8c%e7%bb%b4%e7%a0%81%ef%bc%8c%e5%bc%80%e5%90%af%e7%b4%a7%e5%bc%a0%e3%80%81%e5%88%ba%e6%bf%80%e7%9a%84%e6%8c%91%e6%88%98%e5%90%a7%ef%bc%81';
-            shareWord = shareWord + '&source=bookmark&appkey=&ralateUid=&pic=http%3a%2f%2fwegift.reconnectplatform.com%2fracegame%2foperator%3faction%3dget_score_by_img_code%26img-code%3d';
-            shareWord = shareWord + '15634-405b3886';
-            shareWord = shareWord + '&url=http%3a%2f%2fthenftgame.github.io%2frace%2fw3%2f';
+            // var shareWord = 'http://service.weibo.com/share/share.php?title=';
+            // shareWord = shareWord + '%e6%88%91%e5%9c%a8%40%e5%be%b7%e5%9b%bd%e9%a9%ac%e7%89%8c%e8%bd%ae%e8%83%8e+%23%e8%b5%9b%e8%bd%a6%e6%b8%b8%e6%88%8f%23%e4%b8%ad%e9%a9%b0%e9%aa%8b%e4%ba%86';
+            // shareWord = shareWord + MW.SCORE ;
+            // shareWord = shareWord + '%e7%b1%b3%ef%bc%8c%e5%b9%b6%e6%88%90%e5%8a%9f%e7%95%99%e5%90%8d%e8%8b%b1%e9%9b%84%e6%a6%9c%e3%80%82%e6%83%b3%e7%9f%a5%e9%81%93%e4%bd%a0%e7%9a%84%e9%a9%be%e9%a9%b6%e9%a3%8e%e6%a0%bc%e5%90%97%ef%bc%9f%e6%83%b3%e8%b6%85%e8%bf%87%e6%88%91%ef%bc%8c%e4%b8%8e%e6%88%91%e4%b8%80%e8%be%83%e9%ab%98%e4%b8%8b%e5%90%97%ef%bc%9f%e8%af%b7%e7%82%b9%e5%87%bb%e6%b8%b8%e6%88%8f%e9%93%be%e6%8e%a5%ef%bc%8c%e6%88%96%e6%89%ab%e6%8f%8f%e4%ba%8c%e7%bb%b4%e7%a0%81%ef%bc%8c%e5%bc%80%e5%90%af%e7%b4%a7%e5%bc%a0%e3%80%81%e5%88%ba%e6%bf%80%e7%9a%84%e6%8c%91%e6%88%98%e5%90%a7%ef%bc%81';
+            // shareWord = shareWord + '&source=bookmark&appkey=&ralateUid=&pic=http%3a%2f%2fwegift.reconnectplatform.com%2fracegame%2foperator%3faction%3dget_score_by_img_code%26img-code%3d';
+            // shareWord = shareWord + '15634-405b3886';
+            // shareWord = shareWord + '&url=http%3a%2f%2fthenftgame.github.io%2frace%2fw3%2f';
       
 
-                console.log(shareWord);
-                // $('.getmore').attr('href', shareWord);
-            var shareLink = document.getElementsByClassName('js-sharelink')[0];
-            shareLink.setAttribute('href', shareWord);
-            // 我在@德国马牌轮胎 #赛车游戏#中驰骋了aaa米
-            cc.log('title:' + document.title );
-            document.title = "我在@德国马牌轮胎 #赛车游戏#中驰骋了" + MW.SCORE +"米";
+            //     console.log(shareWord);
+            //     // $('.getmore').attr('href', shareWord);
+            // var shareLink = document.getElementsByClassName('js-sharelink')[0];
+            // shareLink.setAttribute('href', shareWord);
+
 
 
             if (MW.TOP10[9].value01 >= MW.SCORE ){
 
 
-                var lbScore = cc.LabelTTF.create(""+ MW.SCORE + " M","Arial Bold",36);
+                var lbScore = cc.LabelTTF.create(""+MW.SCORE + " M","Arial Bold",36);
                 lbScore.setPosition(160,380);
                 lbScore.setColor(cc.c3b(0,0,0));
                 this.addChild(lbScore,10);
@@ -243,9 +246,6 @@ var GameOver = cc.Layer.extend({
                 // var overMenu = cc.Menu.create(menu1);
                 // overMenu.setPosition(winSize.width / 2 + 140,220);
                 // this.addChild(overMenu);
-                var gamePost = document.createElement('script');
-                gamePost.src = 'http://wegift.reconnectplatform.com/racegame/operator?action=submit_game_score&game_name=RaceGame&player=noname&score='+ MW.SCORE +'&type=a&refresh=1&jsoncallback=jsonpPostback';
-                document.getElementsByTagName('head')[0].appendChild(gamePost);
 
             }else{
                 
@@ -331,16 +331,10 @@ var GameOver = cc.Layer.extend({
     },
     editBoxTextChanged: function (editBox, text) {
         cc.log("editBox " + this._getEditBoxName(editBox) + ", TextChanged, text: " + text);
-        MW.playerName = text;
     },
 
     editBoxReturn: function (editBox) {
         cc.log("editBox " + this._getEditBoxName(editBox) + " was returned !");
-
-        var gamePost = document.createElement('script');
-                gamePost.src = 'http://wegift.reconnectplatform.com/racegame/operator?action=submit_game_score&game_name=RaceGame&player='+ MW.playerName +'&score='+ MW.SCORE +'&type=a&refresh=1&jsoncallback=jsonpPostback';
-                document.getElementsByTagName('head')[0].appendChild(gamePost);
-
     },
     _getEditBoxName :function(editBox){
         if (this._box4 == editBox) {
