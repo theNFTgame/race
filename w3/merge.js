fcdc717537f026bbb2722d13269b58c8823fbd1a
vitrum.zhu <vitrum.cn@gmail.com>
2014-03-10 10:13:35 +0800

    change merge.js

--- w3/merge.js
+++ w3/merge.js
@@ -1,24 +1,24 @@
-varfs=require('fs');
-varuglify=require('uglifyjs');
+var fs=require('fs');
+var uglify=require('uglify-js');
  
-varopen=fs.readFileSync;
+var open = fs.readFileSync;
  
-varconfig=JSON.parse(open('config.json'));
-varcomment=config.comment&&open(config.comment).toString().replace(/%date%/g,newDate().toJSON());
+var config=JSON.parse(open('config.json'));
+var comment=config.comment&&open(config.comment).toString().replace(/%date%/g,newDate().toJSON());
  
-varuglify_option=config.uglify_option||{};
-varinputs=config.input;
-varlf='\n';
+var uglify_option=config.uglify_option||{};
+var inputs=config.input;
+var lf='\n';
  
 if(!inputs||!inputs.length){
 	console.log('no input files!');
 	return;
 }
 // 转化
-varresults=inputs
+var results=inputs
 .map(function(f){
-	varcode=open(f).toString();
-	varoutput=lf+'// '+f+lf+uglify(code,uglify_option);
+	var code=open(f).toString();
+	var output=lf+'// '+f+lf+uglify(code,uglify_option);
 	console.log('uglify... <<< ',f,'\t',output.length+'/',code.length,'\t=',output.length/code.length);
 	return output;
 }).reduce(function(a,b){
